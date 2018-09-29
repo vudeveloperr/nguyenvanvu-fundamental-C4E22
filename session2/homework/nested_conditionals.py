656177b6ca3d7c84d7a5e053c52129fd75ed2664
van vu <vubaojr99@gmail.com>
2018-09-30 00:45:07 +0700

    initial commit

--- session2/homework/nested_conditionals.py
+++ session2/homework/nested_conditionals.py
@@ -4,16 +4,16 @@ n = int(input("enter number : "))
 
 
 if n>0 :   
-    if n%2=0 :
+    if n%2 == 0 :
         print("n la so duong chan")
     else:
         print("n la so duong le")
 
-elif n=0:
+elif n==0:
     print("0 khong la nguyen am khong la nguyen duong") 
 
 else :
-    if n%2=0 :
+    if n%2==0 :
         print("n la so am chan")
     else:
         print("n la so duong le")
